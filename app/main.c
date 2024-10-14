b3dea4a9ce646bb6cddd5328083639044ddb7ca2
mikeromnv <romanosir@yandex.ru>
2024-10-14 14:44:27 +0300

    task 7

--- app/main.c
+++ app/main.c
@@ -3,12 +3,12 @@
 #include <string.h>
 
 #define BUFFER_SIZE 1024 //буфер для ввода
-#define HISTORY_SIZE 100 //для истории
+#define HISTORY_SIZE 1000 //для истории
 #define HISTORY_FILE "command_history.txt"
 
 // Функция для записи истории команд в файл
 void save_history_to_file(char history[][BUFFER_SIZE], int count) {
-    FILE *file = fopen(HISTORY_FILE, "w");
+    FILE *file = fopen(HISTORY_FILE, "a");
     if (file == NULL) {
         perror("Не удалось открыть файл для записи истории");
         return;
@@ -19,6 +19,42 @@ void save_history_to_file(char history[][BUFFER_SIZE], int count) {
     fclose(file);
 }
 
+// чтение бинарника
+void read_binary(const char *filename){
+  FILE *file = fopen(filename, "rb");
+  if (file == NULL){
+    perror ("Не удалось открыть бинарный файл");
+    return;
+  }
+  // определяем размер файла
+  fseek(file, 0, SEEK_END);
+  long file_s = ftell(file);
+  rewind(file); // устанавливает указатель на начло файла
+  
+  unsigned char *buffer = (unsigned char *)malloc(file_s);
+  if (buffer == NULL){
+    perror("Ошибка выделения памяти");
+    fclose(file);
+    return;
+  }
+  
+  size_t count = fread(buffer, 1, file_s, file);
+  if (count != file_s){
+    perror("ошибка чтения данных");
+    free(buffer); //освобождаем память
+    fclose(file);
+    return;
+  }
+  
+  //выводд
+  printf("содержимое бинарного файла %s:\n", filename);
+  for (long i=0;i<file_s;i++){
+    printf("%02X ", buffer[i]);
+  }
+  printf("\n");
+  free(buffer);
+  fclose(file);
+}
 
 int main() {
     char input[BUFFER_SIZE];               // Буфер для ввода команд
@@ -49,14 +85,29 @@ int main() {
             strcpy(history[history_count], input);
             history_count++;
         }
-
+        if (strcmp(input, "echo $PATH") == 0){
+            char *path = getenv("PATH");
+            if (path != NULL){
+                printf("%s\n", path);
+            } else{
+                printf("Переменная PATH не найдена\n");
+            }
+            continue;
+        }
+        //чтение бинарника
+        if (strncmp(input, "cat ", 4) == 0){
+            read_binary(input + 4);
+            continue;
+        }
         // Проверяем команду echo
         if (strncmp(input, "echo ", 5) == 0) {
             printf("%s\n", input + 5); // Выводим всё, что после "echo "
             continue;
         }
-
         // Если это не команда выхода и не echo, просто выводим строку
+        
+        
+        
         printf("Вы ввели: %s\n", input);
     }
     while (!feof(stdin));
