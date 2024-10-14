a8e31483a4569f9e689c4b91d050d842bb036ecb
Mike <102806731+mikeromnv@users.noreply.github.com>
2024-10-14 14:25:01 +0300

    task 1-7

--- app/main.c
+++ app/main.c
@@ -1,29 +1,79 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 #include <string.h>
 
+#define BUFFER_SIZE 1024 //буфер для ввода
+#define HISTORY_SIZE 1000 //для истории
+#define HISTORY_FILE "command_history.txt"
+
+// Функция для записи истории команд в файл
+void save_history_to_file(char history[][BUFFER_SIZE], int count) {
+    FILE *file = fopen(HISTORY_FILE, "a");
+    if (file == NULL) {
+        perror("Не удалось открыть файл для записи истории");
+        return;
+    }
+    for (int i = 0; i < count; i++) {
+        fprintf(file, "%s\n", history[i]);
+    }
+    fclose(file);
+}
+
+
 int main() {
+    char input[BUFFER_SIZE];               // Буфер для ввода команд
+    char history[HISTORY_SIZE][BUFFER_SIZE]; // История команд
+    int history_count = 0;                  // Счётчик команд в истории
 
-  // You can use print statements as follows for debugging, they'll be visible
+    do {
+               
+        printf("MIKE$ ");
+        fflush(stdout);
+        // Чтение ввода с клавиатуры, проверка на EOF (Ctrl+D)
+        if (fgets(input, BUFFER_SIZE, stdin) == NULL) {
+            printf("\nЗавершение работы (Ctrl+D)\n");
+            break; // выход из цикла при EOF
+        }
 
-  // when running tests printf("Logs from your program will appear here!\n");
+        // Убираем символ новой строки, если он есть
+        input[strcspn(input, "\n")] = '\0';
 
-  // Uncomment this block to pass the first stage
+        // Проверяем на команды выхода
+        if (strcmp(input, "exit") == 0 || strcmp(input, "\\q") == 0) {
+            printf("Завершение работы (exit/\\q)\n");
+            break;
+        }
 
-  printf("$ ");
+        // Сохраняем введённую команду в историю (если есть место)
+        if (history_count < HISTORY_SIZE) {
+            strcpy(history[history_count], input);
+            history_count++;
+        }
+        if (strcmp(input, "echo $PATH") == 0){
+            char *path = getenv("PATH");
+            if (path != NULL){
+                printf("%s\n", path);
+            } else{
+                printf("Переменная PATH не найдена\n");
+            }
+            continue;
+        }
+        
+        // Проверяем команду echo
+        if (strncmp(input, "echo ", 5) == 0) {
+            printf("%s\n", input + 5); // Выводим всё, что после "echo "
+            continue;
+        }
+        
+        
+        
 
-  fflush(stdout);
+        // Если это не команда выхода и не echo, просто выводим строку
+        printf("Вы ввели: %s\n", input);
+    }
+    while (!feof(stdin));
+    // Сохраняем историю команд в файл перед выходом
+    save_history_to_file(history, history_count);
 
-  // Wait for user input
-
-  char input[100];
-
-  fgets(input, 100, stdin);
-
-  input[strlen(input) - 1] = '\0';
-
-  printf("%s: command not found\n", input);
-
-  return 0;
-
+    return 0;
 }
