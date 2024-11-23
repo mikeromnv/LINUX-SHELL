b62e8ef2e9512df8d6fdba5dd001a8d118e443be
Mike <102806731+mikeromnv@users.noreply.github.com>
2024-11-23 11:00:36 +0300

    12 task

--- app/main.c
+++ app/main.c
@@ -5,6 +5,13 @@
 #include <signal.h>
 #include <stdint.h>
 #include <unistd.h>
+#include <dirent.h>
+#include <stdbool.h>
+#include <sys/mount.h>
+#include <stdint-gcc.h>
+#include <sys/stat.h>
+
+
 
 #define BUFFER_SIZE 1024 //буфер для ввода
 #define HISTORY_SIZE 1000 //для истории
@@ -112,6 +119,163 @@ void is_bootable_device(char* device_name) {
     }
 }
 
+//cron
+/*
+char* concat(const char *s1, const char *s2) {
+    char *result = malloc(strlen(s1) + strlen(s2) + 1);
+    strcpy(result, s1);
+    memset (s1,'\0',sizeof(s1));
+    strcat(result, s2);
+    return result;
+}
+
+const char* vfs = "/tmp/cronvfs";
+const char* crontab = "/var/spool/cron/crontabs";
+
+void mountVFS(char* SOURCE) {
+    char* MOUNT_POINT = "/tmp/vfs";
+    struct stat st;
+    if (stat(MOUNT_POINT, &st) == -1) {
+        if (mkdir(MOUNT_POINT, 0755) == -1) {
+            printf("Ошибка при создании каталога\n");
+            return;
+        }
+    }
+
+    // Монтируем VFS
+    if (mount(SOURCE, MOUNT_POINT, "vfs", 0, NULL) == -1) {
+        printf("Ошибка при монтировании VFS\n");
+        return;
+    }
+
+    printf("VFS успешно смонтирован в %s\n", MOUNT_POINT);
+    return;
+}
+
+static void cron_create(int i, char *path, char *str) {
+    FILE *f;
+    char *filename;
+
+    sprintf(filename, "task%d", i);
+    concat(path, filename);
+    free(filename);
+
+    f = fopen(path, "w");
+    if (f) {
+        fprintf(f, "%s\n", str);
+        fclose(f);
+    }
+
+}
+
+
+static void cron_list(const char *filename, char *path) {
+    int i = 0;
+    int c;
+    char* str;
+    FILE *f = fopen(filename, "r");
+    if (!f) {
+        perror(filename);
+        return;
+    }
+
+    while ((c = fgetc(f)) != EOF) {
+        if (c != '\n') {
+            concat(&str, (const char *) c);
+            continue;
+        }
+        if (strlen(str) && str[0] != '#') {
+            cron_create(i, path, str);
+            i++;
+        }
+        str = "";
+    }
+    fclose(f);
+}
+
+
+void cron_mount() {
+    int mr;
+
+    mr = mkdir(vfs, 0777);
+    if (mr == -1) {
+        perror(vfs);
+        return;
+    }
+
+    mr = mount("tmpfs", vfs, "tmpfs", 0, NULL);
+    if (mr == -1) {
+        perror("mount");
+        return;
+    }
+}
+
+void cron_umount() {
+    umount(vfs);
+    rmdir(vfs);
+}
+
+void cron() {
+    DIR *dir;
+    struct dirent *dirent;
+    char* path = crontab;
+    char* vfs_path = vfs;
+
+    dir = opendir(crontab);
+    if (!dir) {
+        perror(crontab);
+        return;
+    }
+
+    while ((dirent = readdir(dir)) != NULL) {
+        struct stat buf;
+
+        asprintf(path, "%s", dirent->d_name);
+
+        lstat(path, &buf);
+        if ((buf.st_mode & S_IFMT) == S_IFREG) {
+            cron_list(path, &vfs_path);
+        }
+
+    }
+
+    closedir(dir);
+}
+*/
+
+
+//12 по 'mem <procid>' получить дамп памяти процесса
+bool appendToFile(char* path1, char* path2) {
+    FILE *f1 = fopen(path1, "a");
+    FILE *f2 = fopen(path2, "r");
+    if (!f1 || !f2) {
+        printf("Error while reading file %s\n", path2);
+        return false;
+    }
+    char buf[256];
+
+    while (fgets(buf, 256, f2) != NULL) {
+        fputs(buf, f1);
+    }
+    fclose(f1);
+    fclose(f2);
+    return true;
+}
+
+void makeDump(DIR* dir, char* path) {
+    FILE* res = fopen("res.txt", "w+");
+    fclose(res);
+    struct dirent* ent;
+    char* file_path;
+    while ((ent = readdir(dir)) != NULL) {
+
+        asprintf(&file_path, "%s/%s", path, ent->d_name); // asprintf работает
+        if(!appendToFile("res.txt", file_path)) {
+            return;
+        }
+    }
+    printf("Dump completed!\n");
+}
 
 int main() {
     
@@ -171,6 +335,7 @@ int main() {
               exit(1);
               //continue;
             }
+            sleep(1);
             
         }
         //signal(SIGHUP, handle_SIGHUP);
@@ -184,8 +349,35 @@ int main() {
             char* device_name = input + 3; // Извлечение имени устройства, например, "sda"
 
             is_bootable_device(device_name);
-
+            f = true;
+            continue;
         }
+        
+        //11 "\cron"
+        /*
+        if (strncmp(input, "\\cron ", 5)){
+          mountVFS(input+5);
+          f = true;
+          continue;
+        }
+        */
+        
+        //12
+        if (strncmp(input, "\\proc ", 6) == 0) {
+            char* path;
+            asprintf(&path, "/proc/%s/map_files", input+6);
+
+            DIR* dir = opendir(path);
+            if (dir) {
+                makeDump(dir, path);
+            }
+            else {
+                printf("Process not found\n");
+            }
+            f = true;
+            continue;
+        }
+        
         
         // Проверяем команду echo
         if (strncmp(input, "echo ", 5) == 0) {
