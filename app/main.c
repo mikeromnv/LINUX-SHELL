1eead601c347de37b6f7bef42e25e3be3b3ebc1a
Mike <102806731+mikeromnv@users.noreply.github.com>
2024-11-29 20:28:38 +0300

    Ready tasks

--- app/main.c
+++ app/main.c
@@ -12,64 +12,14 @@
 #include <sys/stat.h>
 
 
+#define _GNU_SOURCE
 
 #define BUFFER_SIZE 1024 //буфер для ввода
-#define HISTORY_SIZE 1000 //для истории
-#define HISTORY_FILE "command_history.txt"
 
-// Функция для записи истории команд в файл
-void save_history_to_file(char history[][BUFFER_SIZE], int count) {
-    FILE *file = fopen(HISTORY_FILE, "a");
-    if (file == NULL) {
-        perror("Не удалось открыть файл для записи истории");
-        return;
-    }
-    for (int i = 0; i < count; i++) {
-        fprintf(file, "%s\n", history[i]);
-    }
-    fclose(file);
-}
-
-//8 чтение бинарника
-void read_binary(const char *filename){
-  FILE *file = fopen(filename, "rb");
-  if (file == NULL){
-    perror ("Не удалось открыть бинарный файл");
-    return;
-  }
-  // определяем размер файла
-  fseek(file, 0, SEEK_END);
-  long file_s = ftell(file);
-  rewind(file); // устанавливает указатель на начло файла
-  
-  unsigned char *buffer = (unsigned char *)malloc(file_s);
-  if (buffer == NULL){
-    perror("Ошибка выделения памяти");
-    fclose(file);
-    return;
-  }
-  
-  size_t count = fread(buffer, 1, file_s, file);
-  if (count != file_s){
-    perror("ошибка чтения данных");
-    free(buffer); //освобождаем память
-    fclose(file);
-    return;
-  }
-  
-  //выводд
-  printf("содержимое бинарного файла %s:\n", filename);
-  for (long i=0;i<file_s;i++){
-    printf("%02X ", buffer[i]);
-  }
-  printf("\n");
-  free(buffer);
-  fclose(file);
-}
-
+// обработчик сигнала sighup
 void handle_SIGHUP(int signal){
   if (signal == SIGHUP){
-    printf("The program interrupted\n");
+    printf("Configuration reloaded\n");
     exit(0);
   }
 }
@@ -92,7 +42,7 @@ void is_bootable_device(char* device_name) {
     FILE* device_file = fopen(full_path, "rb");
     if (device_file == NULL) {
         printf("Не удалось открыть диск %s!\n", device_name);
-        //return;
+        return;
     }
 
     // Переходим к 510 байту
@@ -120,131 +70,10 @@ void is_bootable_device(char* device_name) {
 }
 
 //cron
-/*
-char* concat(const char *s1, const char *s2) {
-    char *result = malloc(strlen(s1) + strlen(s2) + 1);
-    strcpy(result, s1);
-    memset (s1,'\0',sizeof(s1));
-    strcat(result, s2);
-    return result;
-}
-
-const char* vfs = "/tmp/cronvfs";
-const char* crontab = "/var/spool/cron/crontabs";
-
-void mountVFS(char* SOURCE) {
-    char* MOUNT_POINT = "/tmp/vfs";
-    struct stat st;
-    if (stat(MOUNT_POINT, &st) == -1) {
-        if (mkdir(MOUNT_POINT, 0755) == -1) {
-            printf("Ошибка при создании каталога\n");
-            return;
-        }
-    }
-
-    // Монтируем VFS
-    if (mount(SOURCE, MOUNT_POINT, "vfs", 0, NULL) == -1) {
-        printf("Ошибка при монтировании VFS\n");
-        return;
-    }
-
-    printf("VFS успешно смонтирован в %s\n", MOUNT_POINT);
-    return;
-}
-
-static void cron_create(int i, char *path, char *str) {
-    FILE *f;
-    char *filename;
-
-    sprintf(filename, "task%d", i);
-    concat(path, filename);
-    free(filename);
-
-    f = fopen(path, "w");
-    if (f) {
-        fprintf(f, "%s\n", str);
-        fclose(f);
-    }
-
-}
 
 
-static void cron_list(const char *filename, char *path) {
-    int i = 0;
-    int c;
-    char* str;
-    FILE *f = fopen(filename, "r");
-    if (!f) {
-        perror(filename);
-        return;
-    }
-
-    while ((c = fgetc(f)) != EOF) {
-        if (c != '\n') {
-            concat(&str, (const char *) c);
-            continue;
-        }
-        if (strlen(str) && str[0] != '#') {
-            cron_create(i, path, str);
-            i++;
-        }
-        str = "";
-    }
-    fclose(f);
-}
-
-
-void cron_mount() {
-    int mr;
-
-    mr = mkdir(vfs, 0777);
-    if (mr == -1) {
-        perror(vfs);
-        return;
-    }
-
-    mr = mount("tmpfs", vfs, "tmpfs", 0, NULL);
-    if (mr == -1) {
-        perror("mount");
-        return;
-    }
-}
-
-void cron_umount() {
-    umount(vfs);
-    rmdir(vfs);
-}
-
-void cron() {
-    DIR *dir;
-    struct dirent *dirent;
-    char* path = crontab;
-    char* vfs_path = vfs;
-
-    dir = opendir(crontab);
-    if (!dir) {
-        perror(crontab);
-        return;
-    }
-
-    while ((dirent = readdir(dir)) != NULL) {
-        struct stat buf;
-
-        asprintf(path, "%s", dirent->d_name);
-
-        lstat(path, &buf);
-        if ((buf.st_mode & S_IFMT) == S_IFREG) {
-            cron_list(path, &vfs_path);
-        }
-
-    }
-
-    closedir(dir);
-}
-*/
-
-
-//12 по 'mem <procid>' получить дамп памяти процесса
+// 12. по 'mem <procid>' получить дамп памяти процесса
+// Функция для объединения файлов в дамп
 bool appendToFile(char* path1, char* path2) {
     FILE *f1 = fopen(path1, "a");
     FILE *f2 = fopen(path2, "r");
@@ -261,7 +90,7 @@ bool appendToFile(char* path1, char* path2) {
     fclose(f2);
     return true;
 }
-
+// 12. Создание дампа памяти процесса
 void makeDump(DIR* dir, char* path) {
     FILE* res = fopen("res.txt", "w+");
     fclose(res);
@@ -269,7 +98,7 @@ void makeDump(DIR* dir, char* path) {
     char* file_path;
     while ((ent = readdir(dir)) != NULL) {
 
-        asprintf(&file_path, "%s/%s", path, ent->d_name); // asprintf работает
+        asprintf(&file_path, "%s/%s", path, ent->d_name);
         if(!appendToFile("res.txt", file_path)) {
             return;
         }
@@ -278,35 +107,42 @@ void makeDump(DIR* dir, char* path) {
 }
 
 int main() {
+    signal(SIGHUP, handle_SIGHUP);
     
     char input[BUFFER_SIZE];               // Буфер для ввода команд
-    char history[HISTORY_SIZE][BUFFER_SIZE]; // История команд
-    int history_count = 0;                  // Счётчик команд в истории
-    signal(SIGHUP, handle_SIGHUP);
+    
+    FILE *file = fopen("HISTORY.txt", "a");
+    
     do {  
         bool f = false;  
-        printf("MIKE$ ");
+        printf("$ ");
         fflush(stdout);
-        // Чтение ввода с клавиатуры, проверка на EOF (Ctrl+D)
+        
+        // 2. выход по (Ctrl+D)
         if (fgets(input, BUFFER_SIZE, stdin) == NULL) {
             printf("\nЗавершение работы (Ctrl+D)\n");
+            fclose(file);
             break; // выход из цикла при EOF
         }
 
         // Убираем символ новой строки, если он есть
         input[strcspn(input, "\n")] = '\0';
 
-        // Проверяем на команды выхода
+        // 3. Проверяем на команды выхода
         if (strcmp(input, "exit") == 0 || strcmp(input, "\\q") == 0) {
             printf("Завершение работы (exit/\\q)\n");
             break;
         }
 
-        // Сохраняем введённую команду в историю (если есть место)
-        if (history_count < HISTORY_SIZE) {
-            strcpy(history[history_count], input);
-            history_count++;
+        // 4. Сохраняем введённую команду в файл
+        if (file){
+          fprintf(file, "%s\n", input);
+          fclose(file);
+          file = fopen("HISTORY.txt", "a");
         }
+        
+        
+        // 7. Вывод окружения переменной
         if (strcmp(input, "echo $PATH") == 0){
             char *path = getenv("PATH");
             if (path != NULL){
@@ -317,32 +153,28 @@ int main() {
             f = true;
             continue;
         }
-        //чтение бинарника
-        if (strncmp(input, "cat ", 4) == 0){
-            read_binary(input + 4);
+        // 5. Проверяем команду echo
+        if (strncmp(input, "echo ", 5) == 0) {
+            printf("%s\n", input + 5); // Выводим всё, что после "echo "
             f = true;
             continue;
         }
-        //выполнение бинарника
+        // 8. выполнение бинарника
         if (strncmp(input, "run ", 4) == 0){
             pid_t p = fork();
             if (p == 0){
               char *argv[] = { "sh", "-c", input + 4, 0 };
               execvp(argv[0], argv);
               fprintf(stderr, "Failed to exec shell on %s\n", input + 4);
-            
               f = true;
               exit(1);
-              //continue;
+              
             }
             sleep(1);
-            
+            continue;
         }
-        //signal(SIGHUP, handle_SIGHUP);
-        
-        
-        
-        // 10 определить является ли диск загрузочным
+      
+        // 10. определить является ли диск загрузочным
         if (strncmp(input, "\\l", 2) == 0) {
             // Проверка, является ли диск загрузочным
 
@@ -354,15 +186,10 @@ int main() {
         }
         
         //11 "\cron"
-        /*
-        if (strncmp(input, "\\cron ", 5)){
-          mountVFS(input+5);
-          f = true;
-          continue;
-        }
-        */
         
-        //12
+        
+        
+        //12. получить дамп памяти по id процесса
         if (strncmp(input, "\\proc ", 6) == 0) {
             char* path;
             asprintf(&path, "/proc/%s/map_files", input+6);
@@ -378,28 +205,12 @@ int main() {
             continue;
         }
         
-        
-        // Проверяем команду echo
-        if (strncmp(input, "echo ", 5) == 0) {
-            printf("%s\n", input + 5); // Выводим всё, что после "echo "
-            f = true;
-            continue;
-        }
+        // 6. Отсутствие команды
         if (f == false){
-          printf("There is no such command!\n");
-          
-        }
-        
-        
-        
-        
+          printf("There is no such command!\n"); 
+        } 
     }
     while (!feof(stdin));
-    // Сохраняем историю команд в файл перед выходом
-    save_history_to_file(history, history_count);
-
     return 0;
 }
 
-//execv - бинарник
-//fork
